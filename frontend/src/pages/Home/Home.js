d34fab23a4a85a3303f2e12aed3a6d31831c4426
dhinakaran2425 <dhinakaran2425@gmail.com>
2024-07-10 00:06:16 +0530

    IMS

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { RiProductHuntLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import heroImg from "../../assets/inv-img.png";
+import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+      <div className="logo">
+  A
+</div>
+
+
+        <ul className="home-links">
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+      {/* HERO SECTION */}
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Arulmurugan Coirs</h2>
+          <h2>Inventory  Management System</h2>
+          <div className="hero-buttons">
+           
+          </div>
+          <div className="--flex-start">
+            <NumberText num="14K" text="Brand Owners" />
+            <NumberText num="23K" text="Active Users" />
+            <NumberText num="500+" text="Partners" />
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImg} alt="Inventory" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
+};
+
+export default Home;
